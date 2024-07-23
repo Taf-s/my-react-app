ae00d9c49868d4c70261deced4b52e539b930445
taf-s <tafara.sithole@gmail.com>
2024-07-23 10:54:12 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./components/layout";
+import Layout from "./components/layout";
+function App() {
+  return (
+    <div>
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div className="layout">
+      <h1 className="title">
+        {" "}
+        Hello World, My name is Tafara and I am a Software Developer
+      </h1>
+    </div>
+  );
+};
+
+export default Layout;
