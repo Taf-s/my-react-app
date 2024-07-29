ea275160a4bf3d732e73b565a6c8aea6c567e757
taf-s <tafara.sithole@gmail.com>
2024-07-29 17:03:03 +0200

    added services section

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,11 @@ const Layout = () => {
       <div className="heading">
         <h1 className="title">Tafara Sithole</h1>
       </div>
+      <div className="services">
+        <h1 className="services-title">Services</h1>
+        <h2 className="subtitle-1">Software Development</h2>
+        <h3 className="subtitle-2">Brand Consulting</h3>
+      </div>
     </div>
   );
 };
