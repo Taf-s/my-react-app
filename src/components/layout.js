dc05df8a34274b819da023d727ff71cca263418c
taf-s <tafara.sithole@gmail.com>
2024-07-29 16:28:39 +0200

    changes background design and header element

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,9 @@ import React from "react";
 const Layout = () => {
   return (
     <div className="layout">
-      <h1 className="title">
-        {" "}
-        Hello World, My name is Tafara and I am a Software Developer
-      </h1>
+      <div className="heading">
+        <h1 className="title">Tafara Sithole</h1>
+      </div>
     </div>
   );
 };
